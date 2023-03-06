97b2b65f16191c460673d7e08c8164711487be84
airi-kinjo-rh <airi_kinjo@reazon.jp>
2023-03-06 12:13:56 +0900

    -7.3

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: { name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"}}
+    end
+    assert_response :unprocessable_entity
+    # /users/new.html.erb が描画されているかどうかを検証する
+    assert_template 'users/new'
+    # アクション実行後に描写されるHTML要素内に、キーが存在するかどうかを検証する
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+  end
+end
