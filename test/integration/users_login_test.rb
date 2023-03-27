e0e954b696e952f40509d39e2a7cf42bffdfe8bf
airi-kinjo-rh <airi_kinjo@reazon.jp>
2023-03-27 16:53:52 +0900

    -8.2

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,25 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: ""}}
+    assert_response :unprocessable_entity
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "michael@example.com", password: "invalid"}}
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
